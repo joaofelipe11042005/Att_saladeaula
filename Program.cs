ca462cbbc23f168429b3aa62b09cde83daab49cf
joaofelipe11042005 <joaofelipe2034@gmail.com>
2026-02-25 20:11:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+double x1, y1, x2, y2, d;
+
+Console.WriteLine("digite o valor de x1");
+x1 = Convert.ToDouble((Console.ReadLine));
+Console.WriteLine("digite o valor de x2");
+x2 = Convert.ToDouble((Console.ReadLine));
+Console.WriteLine("digite o valor de y1");
+y1 = Convert.ToDouble((Console.ReadLine));
+Console.WriteLine("digite o valor de y2");
+y2 = Convert.ToDouble((Console.ReadLine));
+d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("o valor dos ponos e" + d);
+Console.ReadKey();
